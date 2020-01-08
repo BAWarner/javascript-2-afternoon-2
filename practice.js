d9571365f08475c75a5ded69e4eb66c99efa70b1
Brandon Warner <warner.brandon.a@gmail.com>
2020-01-08 15:55:57 -0600

    Finish all but practice

--- practice.js
+++ practice.js
@@ -17,7 +17,9 @@ var arr = [10,20,30];
   Return the first item in the given array.
 */
 
-//Code Here
+function first(arr){
+  return arr[0];
+}
 
 
 
@@ -32,7 +34,9 @@ var arr = [40,50,60];
   Return the last item in the given array.
 */
 
-//Code Here
+function last(arr){
+  return arr[arr.length - 1];
+}
 
 
 
@@ -47,8 +51,11 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
   Loop through the given array and alert every item in the array.
 */
 
-//Code Here
-
+function looper(family){
+  for(let i = 0; i < family.length; i++){
+    alert(family[i]);
+  }
+}
 
 
 ////////// PROBLEM 4 //////////
@@ -62,7 +69,11 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
   Loop backwards, starting at the end of the letters array, alerting every item in the array.
 */
 
-//Code Here
+function reversedLooper(letters){
+  for(let i = letters.length -1; i >= 0; i--){
+    alert(letters[i]);
+  }
+}
 
 
 
@@ -77,7 +88,15 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
   Return an array that contains the even numbers from the nums array.
 */
 
-//Code Here
+function evenFinder(nums){
+  let evenNumbers = [];
+  for(let i = 0; i < nums.length; i++){
+    if(nums[i] % 2 == 0){
+      evenNumbers.push(nums[i]);
+    }
+  }
+  return evenNumbers;
+}
 
 
 
@@ -106,7 +125,24 @@ var numbersArray = [1,2,34,54,55,34,32,11,19,17,54,66,13];
   and the second item in the array being the odds array (all the odd values from numbersArray).
 */
 
-//Code Here
+function divider(numbersArray){
+  let twoArraysArray = [];
+  let evensArray = [];
+  let oddsArray = [];
+
+  for(let i = 0; i < numbersArray.length; i++){
+    if(numbersArray[i] % 2 === 0){
+      evensArray.push(numbersArray[i]);
+
+    }else{
+      oddsArray.push(numbersArray[i]);
+    }
+  }
+  twoArraysArray.push(evensArray);
+  twoArraysArray.push(oddsArray);
+
+  return twoArraysArray;
+}
 
 
 
@@ -128,7 +164,12 @@ var getRandomArbitrary = function() {
   If it is, return true, if it's not, return false
 */
 
-//Code Here
+function finder(arr){
+  var randomNumber = getRandomArbitrary();
+  for(let i = 0; i < arr.length; i++){
+
+  }
+}
 
 
 
@@ -157,9 +198,11 @@ var myGroceryList = ['chips', 'pizza', 'hotpockets', 'MtnDew', 'corndogs'];
   addItem() --> [];
 */
 
-//Code Here
 
 
+function removeItem(myGroceryList, item){
+
+}
 
 ////////// PROBLEM 9 //////////
 
@@ -167,7 +210,14 @@ var myGroceryList = ['chips', 'pizza', 'hotpockets', 'MtnDew', 'corndogs'];
   Write a function called maker that creates an array, fills that array with numbers from 1 to 215, then returns the array.
 */
 
-//Code Here
+function maker(){
+  let longArray = [];
+  for(let i = 0; i <= 215; i++){
+    longArray.push(i);
+  }
+  longArray.shift();
+  return longArray;
+}
 
 
 
@@ -183,7 +233,15 @@ var numbers = [5, '9', 16, 19, '25', '34', 48];
   Your output should look like this -> [15, 19, 26, 29, 35, 44, 58]
 */
   
-//Code Here
+function addTen(numbers){
+  let newArray = [];
+  for(let i = 0; i < numbers.length; i++){
+    var stringToNumber = parseInt(numbers[i], 10);
+    var addition = stringToNumber + 10;
+    numbers[i] = addition;
+  }
+  return newArray;
+}
 
 
 
@@ -208,7 +266,7 @@ for(var i = 0; i < num2; i++){
   Return the longer of the two arrays.
 */
 
-//Code Here
+
 
 
 
